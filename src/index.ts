3de90fed2d9d5df7aa676f87a5660244bcd9cf97
taoqf <tao_qf@163.com>
2020-03-09 10:17:50 +0800

    remove dep, add header content-type

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,22 @@
-import aw3 from '@mmstudio/aw000003';
 import global from '@mmstudio/global';
 
-export default function send_msg<T>(service: string, msg: unknown) {
+export default async function send_msg<T>(service: string, msg: unknown) {
 	const data = JSON.stringify(msg);
 	const url = `${global('host', '.')}/sendmessage/${encodeURIComponent(service)}`;
-	return aw3<T>(url, data);
+
+	const res = await fetch(url, {
+		method: 'POST',
+		body: data,
+		headers: {
+			'Content-Type': 'text/json; charset=utf-8'
+		}
+	});
+	const content_type = res.headers.get('Content-type');
+	if (res.status > 0 && res.status < 400) {
+		if (content_type && /json/i.test(content_type)) {
+			return await res.json() as T;
+		}
+		return res.text() as unknown as T;
+	}
+	throw new Error(res.statusText);
 }
